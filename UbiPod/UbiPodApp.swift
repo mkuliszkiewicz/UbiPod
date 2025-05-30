69cea4cfe98878f83a0bb41cb620382694ec650e
Maciej Kuliszkiewicz <maciej.kuliszkiewicz@volvocars.com>
2025-05-30 13:15:33 +0200

    Initial Commit

---
+++ UbiPod/UbiPodApp.swift
@@ -0,0 +1,17 @@
+//
+//  UbiPodApp.swift
+//  UbiPod
+//
+//  Created by Maciej Banasiewicz on 2025-05-30.
+//
+
+import SwiftUI
+
+@main
+struct UbiPodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
